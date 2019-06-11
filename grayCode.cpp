52207a8f45dcd0e3530450259f7748937e2741a6
imcoder23 <deependraparajuli@gmail.com>
2019-06-11 11:09:21 -0700

    Gray-Code

---
+++ grayCode.cpp
@@ -0,0 +1,19 @@
+
+   void findGrayCode(vector<int> &res, int n, int &num){
+    if(n==0){
+        res.push_back(num);
+        return;
+    }
+       findGrayCode(res,n-1,num);
+       num = num ^(1 << (n-1));
+       findGrayCode(res,n-1,num);
+   } 
+
+    vector<int> grayCode(int n) {
+        vector <int> res;   
+        int num = 0;
+        findGrayCode(res, n, num);
+        return res;
+    }
+
+
